9a04ab59cdd2a71846e914db683f302d9ba79d10
Armando Ramirez <armando.p.ramirez@gmail.com>
2017-08-09 19:04:15 -0300

    added restaurants reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurant_attributes = [
+{
+  name: "Verdusco",
+  address: "Dixon",
+  description: "good mexican food",
+  stars: 3,
+  chef: "El ruco",
+},
+{
+  name: "Pollo Loco",
+  address: "Vacaville",
+  description: "Chicken Legs",
+  stars: 2,
+  chef: "John Venice",
+},
+{
+  name: "La Quicita",
+  address: "Soul food",
+  description: "bad beans for you",
+  stars: 1,
+  chef: "Mata Pollos",
+},
+{
+  name: "Molcajete",
+  address: "Vallejo",
+  description: "The best Mexican food",
+  stars: 2,
+  chef: "el macote",
+},
+{
+  name: "Tahoe Joes",
+  address: "San Francisco",
+  description: "American steaks",
+  stars: 4,
+  chef: "la rana",
+},
+]
+restaurant_attributes.each { |params| Restaurant.create!(params)}
+
